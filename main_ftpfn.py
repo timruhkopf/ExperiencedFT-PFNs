24d8d4a45da7608763cdc52e517d9f214cc8993f
timruhkopf <timruhkopf@gmail.com>
2025-05-09 20:33:28 +0200

    [minor] for the read to automatically be triggered correctly, we need to rename the log here

--- main_ftpfn.py
+++ main_ftpfn.py
@@ -37,7 +37,7 @@ logger = logging.getLogger(__name__)
 
 @hydra.main(config_path="configs", config_name="base_ft", version_base="1.1")
 def main(cfg: DictConfig):
-    logger.info(f'Sweep dir:: {Path.cwd()}')
+    logger.info(f'Sweep dir: {Path.cwd()}')
     logger.info(f"Running with config: \n {OmegaConf.to_yaml(cfg, resolve=True)}")
     # fixme: device
     device =  torch.device("cuda" if torch.cuda.is_available() else "cpu") if cfg.device is None \
