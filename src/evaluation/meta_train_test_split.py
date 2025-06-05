1366a5255882ac43dffd6f845f6f02798f0d4166
timruhkopf <timruhkopf@gmail.com>
2025-06-05 13:38:19 +0200

    [feature] trying out all meta-train datasets in one go to increase the likelihood of relateable experiences

--- src/evaluation/meta_train_test_split.py
+++ src/evaluation/meta_train_test_split.py
@@ -11,8 +11,13 @@ def k_folds(train_ids, k=5):
 
 def folds_of_size(train_ids, size=5, drop=True):
     """Split train_ids into non overlapping folds of at most size"""
+
+    if size <= 0:
+        return train_ids
+
     if drop:
         n = len(train_ids) // size
     else:
         n =  int(math.ceil(len(train_ids) / size))
+
     return [train_ids[i*size:(i+1)*size] for i in range(n)]
