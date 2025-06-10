4d4b21af554062c44ee70c80df87e1cac31bd53b
timruhkopf <timruhkopf@gmail.com>
2025-06-10 14:24:16 +0200

    [feature] linear regression scale adjustment between tasks

--- src/model/ifbo.py
+++ src/model/ifbo.py
@@ -60,6 +60,8 @@ class MyBaseModel(BaseModel):
         sys.modules["ifbo"] = ifbo
         spec.loader.exec_module(ifbo)
 
+        #        with torch.serialization.safe_globals([ifbo.transformer.TransformerModel]):
+
         self.model = torch.load(
                 path, map_location=torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
                 weights_only=False
