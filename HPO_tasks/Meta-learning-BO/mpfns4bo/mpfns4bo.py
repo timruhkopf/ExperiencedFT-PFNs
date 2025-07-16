fix normalization

HPO_tasks/Meta-learning-BO/mpfns4bo/mpfns4bo.py
@@ -43,7 +43,7 @@ class MPFNs4BO(nn.Module):
             padding_mask.append(np.concatenate([np.zeros(item["y"].shape[0]),  np.ones(max_length- item["y"].shape[0])]  ))
         x_task_context = to_tensor(np.stack(x_task_context, axis=1)).to(torch.float32).to(device)
         y_task_context =  to_tensor(np.stack(y_task_context, axis=1)).to(torch.float32).to(device)
-        y_task_context =  self.label_transforms(y_task_context, **self.kwargs)
+        #y_task_context =  self.label_transforms(y_task_context, **self.kwargs)
         padding_mask =  to_tensor(np.stack(padding_mask, axis=1)).to(torch.bool).to(device).T 
 
         self.related_task_data = SimpleNamespace(x=x_task_context, y=y_task_context, padding_mask=padding_mask)

HPO_tasks/Meta-learning-BO/mpfns4bo/pfns4bo_opt.py
@@ -37,7 +37,7 @@ class PFNs4BO:
 
         with (torch.cuda.amp.autocast() if self.device[:3] != 'cpu' else contextlib.nullcontext()):
             acq_values = self.acq_function(self.model, X_obs, y_obs,
-                                           X_pen, return_actual_ei=return_actual_ei, **self.kwargs).cpu().clone()  # bool array
+                                           X_pen, return_actual_ei=return_actual_ei, apply_power_transform = False, **self.kwargs).cpu().clone()  # bool array
             acq_mask = acq_values.max() == acq_values
         possible_next = torch.arange(len(X_pen))[acq_mask]
         if len(possible_next) == 0: