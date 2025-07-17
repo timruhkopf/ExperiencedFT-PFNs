3b53bc77213bf5ee2cd23d6a73c5fb442ea42484
timruhkopf <timruhkopf@gmail.com>
2025-07-17 13:55:01 +0200

    [bug-fix] folds_of_size was returning the entire array regardless.

--- src/evaluation/meta_train_test_split.py
+++ src/evaluation/meta_train_test_split.py
@@ -15,12 +15,12 @@ def folds_of_size(train_ids, size=5, drop=True):
     if size <= 0:
         return [train_ids]
 
+    if size == len(train_ids):
+        return [train_ids]
+
     if drop:
         n = len(train_ids) // size
     else:
         n =  int(math.ceil(len(train_ids) / size))
-
-    if size > n :
-        return [train_ids]
 
     return [train_ids[i*size:(i+1)*size] for i in range(n)]
