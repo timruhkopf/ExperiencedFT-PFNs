d241818ae0c9dceda9da35e5245e9b710847f370
Amir Balef <amirbalef@gmail.com>
2025-07-22 11:55:16 +0200

    adding input warping

--- HPO_tasks/Meta-learning-BO/main.py
+++ HPO_tasks/Meta-learning-BO/main.py
@@ -107,6 +107,13 @@ if __name__ == "__main__":
                     from mpfns4bo.pfns4bo_opt import PFNs4BO
                     optimizer = PFNs4BO(torch.load( pfns4bo.hebo_plus_model), device=args.device, acq_function_type='ei')
 
+                elif args.method == "PFNs4BO-EI-IW":
+                    import pfns4bo
+                    from mpfns4bo.pfns4bo_opt import PFNs4BO
+                    from pfns4bo.scripts.tune_input_warping import fit_input_warping
+                    from botorch.models.transforms.utils import expand_and_copy_tensor
+                    optimizer = PFNs4BO(torch.load( pfns4bo.hebo_plus_model), fit_encoder=fit_input_warping, device=args.device, acq_function_type='ei')
+
                 elif args.method == "PFNs4BO-PI":
                     import pfns4bo
                     from mpfns4bo.pfns4bo_opt import PFNs4BO
