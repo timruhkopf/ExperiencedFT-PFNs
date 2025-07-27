fix normalization

HPO_tasks/Meta-learning-BO/mpfns4bo/mpfns4bo.py
@@ -86,7 +86,7 @@ class MPFNs4BO(nn.Module):
             only_obs_incumbents=self.only_obs_incumbents
         )
     def normalize(self, y):
-        return (y-np.min(y))/(np.max(y)-np.min(y))
+        return (y-np.min(y))/(np.max(y)-np.min(y)+ 1e-8)
 
     @torch.no_grad()
     def observe_and_suggest(self, X_obs, y_obs, X_pen, return_actual_ei=False, minimize=True):

src/model/mixing.py
@@ -47,9 +47,9 @@ class myCVMixtureStrategy:
             )
             related_nll = related_nll.to(device)
         else:
-            # uniform scores
+            # only relay on the target scores
             num_related = pi_related.shape[0]
-            target_nll = torch.zeros(1, device=device)
+            target_nll = torch.ones(1, device=device)
             related_nll = torch.zeros(num_related, device=device)
 
         # weigh the target and related scores by the reliability