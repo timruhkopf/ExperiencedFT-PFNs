43402c5e4094f3d602acf3b2578f26214c1c6ddd
Amir Balef <amirbalef@gmail.com>
2025-07-27 11:26:10 +0200

    fix normalization

--- HPO_tasks/Meta-learning-BO/run_scripts/submit_6_instances.sh
+++ HPO_tasks/Meta-learning-BO/run_scripts/submit_6_instances.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 search_space_ids=('4796' '5860' '5906'  '5527'  '5889'  '5859')
-search_space_ids=("5859") #('4796' '5889'  '5859')
+search_space_ids=('4796' '5889'  '5859')
 #9 test 5965, 7609, 5889, 6794, 5859, 4796, 7607, 5636, and 5970.
 #9 valid 5527, 5891, 5906, 5971, 6767, 6766, and 5860
-policies=("pPFNs4BO-pp" "pPFNs4BO-sp") #("PFNs4BO-PI-o" "PFNs4BO-PI" "PFNs4BO-PI-IW" "PFNs4BO-EI-IW") #  "mPFNs4BO" "MALIBO"
+policies=("pPFNs4BO-pp" "pPFNs4BO-dp") #("PFNs4BO-PI-o" "PFNs4BO-PI" "PFNs4BO-PI-IW" "PFNs4BO-EI-IW") #  "mPFNs4BO" "MALIBO"
 evaluations=60
 
 seeds=("test0" "test1" "test2" "test3" "test4")
